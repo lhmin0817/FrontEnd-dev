cd827bc3c92c26102f8fff217b09b4c14c687188
Hwanmin <qwop987@naver.com>
2023-01-02 18:24:29 +0900

    1122334455

---
+++ FrontEnd/11/js/ejs.js
@@ -0,0 +1,5 @@
+var age=23;
+/*자바 스크립트를 사용해서 html 문서의 스타일 적용 */
+document.write("<div style='color:red; font-size:24px;'>외부 자바스크립트 파일</div>");
+document.write('<div style="color:red; font-size:24px;">외부 자바스크립트 파일</div>');
+document.write("당신의 나이는 : "+age+"입니다.");
